7ca8822232d683cfc317912430bc90ba9d35249a
Ashutosh1124 <ashutoshtiwari2585@gmail.com>
2025-07-11 20:18:34 +0000

    some details about variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountName = "John Doe";
+var accountBalance = "100";
+
+accountCity = "New York"; 
+
+// accountId= 67890; // Reassigning a constant will throw an error
+
+console.log(accountId); 
+
